cf7619f8a27c011d6aa63a2298aad9cf41142ddf
rudyjm3 <rudyjm3@yahoo.com>
2021-09-07 06:24:05 -0400

    added pulse effect to the plus button

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ document.getElementById("menu").style.transform = "scale(3)";
   }
 }
 
+
 // DRAGABLE MENU
 //var dragValue;
 
@@ -38,9 +39,9 @@ dragElement(document.getElementById("menu-container"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  if (document.getElementById(elmnt.id + "plus")) {
     /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id + "plus").onmousedown = dragMouseDown;
   } else {
     /* otherwise, move the DIV from anywhere inside the DIV:*/
     elmnt.onmousedown = dragMouseDown;
